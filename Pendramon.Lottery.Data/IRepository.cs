efa10eb65696917090772942f0aa56f7c375726f
maxki423 <maxki423@hotmail.com>
2018-09-01 12:27:38 +0200

    Implemented repository pattern

---
+++ Pendramon.Lottery.Data/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using Pendramon.Lottery.Data.Model.Interfaces;
+using System.Linq;
+
+namespace Pendramon.Lottery.Data
+{
+    public interface IRepository<T> where T : IEntity
+    {
+        void Insert(T entity);
+        void Delete(T entity);
+        IQueryable<T> GetAll();
+        T GetById(int id);
+    }
+}
---
+++ Pendramon.Lottery.Data/Repository.cs
@@ -0,0 +1,37 @@
+﻿
+using System.Linq;
+using Pendramon.Lottery.Data.Model.Interfaces;
+using System.Data.Entity;
+
+namespace Pendramon.Lottery.Data
+{
+    public class Repository<T> : IRepository<T> where T : class, IEntity
+    {
+        protected DbSet<T> DbSet;
+        
+        public Repository(DbContext dbContext)
+        {
+            DbSet = dbContext.Set<T>();
+        }
+
+        public void Insert(T entity)
+        {
+            DbSet.Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            DbSet.Remove(entity);
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return DbSet;
+        }
+
+        public T GetById(int id)
+        {
+            return DbSet.Find(id);
+        }
+    }
+}
