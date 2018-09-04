9b9af0cf2542c016157b6e07a13b86950ab3aebd
maxki423 <maxki423@hotmail.com>
2018-09-04 20:50:44 +0200

    Created Lottery Controller

---
+++ Pendramon.Lottery.Web/Controllers/LotteryController.cs
@@ -0,0 +1,36 @@
+﻿using Pendramon.Lottery.Service.Interfaces;
+using Pendramon.Lottery.View.Model;
+using System.Web.Http;
+
+namespace Pendramon.Lottery.Web.Controllers
+{
+    public class LotteryController : ApiController
+    {
+
+        #region Private Members
+
+        private readonly ILotteryService lotteryService;
+
+        #endregion
+
+        #region Constructor
+
+        public LotteryController(ILotteryService lotteryService)
+        {
+            this.lotteryService = lotteryService;
+        }   
+
+        #endregion
+
+        #region Public Methods
+
+        [HttpPost]
+        public AwardModel SubmitCode([FromBody] UserCodeModel userCodeModel)
+        {
+            return lotteryService.CheckCode(userCodeModel);
+        }
+
+        #endregion
+
+    }
+}
