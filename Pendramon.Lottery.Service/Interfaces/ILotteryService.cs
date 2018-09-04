3d4925e4aca4a8f67bfa1f233be188387471ad74
maxki423 <maxki423@hotmail.com>
2018-09-04 19:21:01 +0200

    Created Lottery Service

---
+++ Pendramon.Lottery.Service/Interfaces/ILotteryService.cs
@@ -0,0 +1,9 @@
+﻿using Pendramon.Lottery.View.Model;
+
+namespace Pendramon.Lottery.Service.Interfaces
+{
+    public interface ILotteryService
+    {
+        AwardModel CheckCode(UserCodeModel userCode);
+    }
+}
