b49a08cb68a039acd9e8baace9b50d4fc20f5a21
maxki423 <maxki423@hotmail.com>
2018-09-04 20:50:27 +0200

    Implemented Dependency Injection

---
+++ Pendramon.Lottery.Web/App_Start/IocConfig.cs
@@ -0,0 +1,36 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using Pendramon.Lottery.Service;
+using System.Reflection;
+using System.Web.Http;
+
+namespace Pendramon.Lottery.Web.App_Start
+{
+    public class IocConfig
+    {
+        public static IContainer Container;
+
+        public static void Initialize(HttpConfiguration config)
+        {
+            Initialize(config, RegisterDependencies(new ContainerBuilder()));
+        }
+
+        public static void Initialize(HttpConfiguration config, IContainer container)
+        {
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+
+        private static IContainer RegisterDependencies(ContainerBuilder builder)
+        {
+            //Register your web API controllers.
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            builder.RegisterType<LotteryService>().As<LotteryService>().InstancePerRequest();
+
+            //builder.RegisterModule(new ServiceModule());
+
+            return builder.Build();
+        }
+
+    }
+}
--- Pendramon.Lottery.Web/Global.asax.cs
+++ Pendramon.Lottery.Web/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Pendramon.Lottery.Web.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,7 @@ namespace Pendramon.Lottery.Web
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            IocConfig.Initialize(GlobalConfiguration.Configuration);
         }
     }
 }
